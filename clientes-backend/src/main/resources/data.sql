c1b14a1f8a496b2dcb6c71ff5b63e8a76ac39315
Tao Hansen <taohansens@gmail.c>
2020-10-03 15:22:11 -0300

    H2 and Database Seeding ok

---
+++ clientes-backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Marlene Jéssica Brenda Dias','81533379637',1045, TIMESTAMP WITH TIME ZONE '1998-07-13T20:50:07.12345Z',0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Fernando Matheus Vieira','93747070981',2080, TIMESTAMP WITH TIME ZONE '2002-01-20T20:50:07.12345Z',2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Emanuel Leonardo Tomás Porto','44128205214',1860, TIMESTAMP WITH TIME ZONE '1984-04-09T20:50:07.12345Z',3);
