4a4be60e313abfbd178081c5b305ac230ae91be3
Tao Hansen <taohansens@gmail.c>
2020-10-03 14:15:31 -0300

    Project created

---
+++ clientes-backend/src/main/java/com/dsbootacamptaoh/clientesbackend/ClientesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dsbootacamptaoh.clientesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesBackendApplication.class, args);
+	}
+
+}
