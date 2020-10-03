c294c12e3304d58ee0f02644185fd395567c9f17
Tao Hansen <taohansens@gmail.c>
2020-10-03 15:29:31 -0300

    Client DTO and Service ok. Returning List of ClientDTO

--- clientes-backend/src/main/java/com/dsbootacamptaoh/clientesbackend/resources/ClientResource.java
+++ clientes-backend/src/main/java/com/dsbootacamptaoh/clientesbackend/resources/ClientResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dsbootacamptaoh.clientesbackend.entities.Client;
+import com.dsbootacamptaoh.clientesbackend.DTO.ClientDTO;
 import com.dsbootacamptaoh.clientesbackend.services.ClientService;
 
 @RestController
@@ -19,8 +19,8 @@ public class ClientResource {
 	private ClientService service;
 
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = service.findAll();
+	public ResponseEntity<List<ClientDTO>> findAll() {
+		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
